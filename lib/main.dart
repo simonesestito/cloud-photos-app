e2cbd6e03901b7077a655328dd01971f0850a388
Simone Sestito <simone@simonesestito.com>
2024-04-20 10:07:21 +0200

    Create a global theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:cloud_photos_app/preferences/preferences.dart';
 import 'package:cloud_photos_app/screen/home_screen.dart';
 import 'package:cloud_photos_app/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -20,10 +21,7 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: 'Photos App',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lime),
-        useMaterial3: true,
-      ),
+      theme: _createTheme(),
       debugShowCheckedModeBanner: false,
       initialRoute: hasLogin ? '/home' : '/login',
       routes: {
@@ -32,4 +30,37 @@ class MyApp extends StatelessWidget {
       },
     );
   }
+
+  ThemeData _createTheme() => ThemeData(
+        colorScheme: _createColorScheme(),
+        elevatedButtonTheme: _createElevatedButtonTheme(),
+        searchBarTheme: _createSearchBarTheme(),
+        inputDecorationTheme: _createInputDecoration(),
+      );
+
+  ColorScheme _createColorScheme() =>
+      ColorScheme.fromSeed(seedColor: Colors.green);
+
+  ElevatedButtonThemeData _createElevatedButtonTheme() =>
+      const ElevatedButtonThemeData(
+        style: ButtonStyle(
+          padding: MaterialStatePropertyAll(EdgeInsets.all(24)),
+          elevation: MaterialStatePropertyAll(4),
+        ),
+      );
+
+  SearchBarThemeData _createSearchBarTheme() => const SearchBarThemeData(
+        elevation: MaterialStatePropertyAll(0),
+        shape: MaterialStatePropertyAll(
+          RoundedRectangleBorder(
+            side: BorderSide(),
+            borderRadius: BorderRadius.all(Radius.circular(8)),
+          ),
+        ),
+      );
+
+  InputDecorationTheme _createInputDecoration() => const InputDecorationTheme(
+        border: OutlineInputBorder(),
+        filled: true,
+      );
 }
