2b06151c9e255b3842e22890742b1e0704169993
Simone Sestito <simone@simonesestito.com>
2024-04-29 19:00:49 +0200

    Use Load Balancer for reaching backend on AWS (still HTTP)

--- lib/repository/aws.dart
+++ lib/repository/aws.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 
 final awsHttpClient = Dio(BaseOptions(
-  baseUrl: 'http://54.196.227.223',
+  baseUrl: 'http://cloudprod-elb-1744064738.us-east-1.elb.amazonaws.com/',
   contentType: 'application/json',
 ));
