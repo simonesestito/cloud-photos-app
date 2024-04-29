682b43c42bbcf43e9b0a15a0f3907545b0f30ea7
Simone Sestito <simone@simonesestito.com>
2024-04-29 17:35:14 +0200

    Update AWS instance IP

--- lib/repository/aws.dart
+++ lib/repository/aws.dart
@@ -1,6 +1,6 @@
 import 'package:dio/dio.dart';
 
 final awsHttpClient = Dio(BaseOptions(
-  baseUrl: 'http://54.221.3.189',
+  baseUrl: 'http://54.196.227.223',
   contentType: 'application/json',
 ));
